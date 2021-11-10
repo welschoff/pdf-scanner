b0e510bba7668ea5553a1babcde66122a30a6e42
welschoff <h.welschoff@gmx.de>
2021-11-10 21:50:31 +0100

    :lipstick: Style Scan Page

--- src/app/components/AddDocumentForm/AddDocumentForm.tsx
+++ src/app/components/AddDocumentForm/AddDocumentForm.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
+import styles from './AddDocumentForm.styles.module.css';
 
 function AddDocumentForm() {
   const [title, setTitle] = useState('');
 
   return (
-    <form>
+    <form className={styles.form}>
       <input
         type="text"
         placeholder="Enter title"
+        className={styles.input}
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
-      <input type="submit" />
+      <input type="submit" className={styles.submit} value="Add" />
     </form>
   );
 }
--- src/app/components/Progress/Progress.tsx
+++ src/app/components/Progress/Progress.tsx
@@ -6,7 +6,7 @@ function Progress({ status, progress }: RecognizeProgress): JSX.Element {
   return (
     <aside className={styles.container}>
       {status}
-      <progress value={progress * 100} max={100} />
+      <progress className={styles.progress} value={progress * 100} max={100} />
     </aside>
   );
 }
