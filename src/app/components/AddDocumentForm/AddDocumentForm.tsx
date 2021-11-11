cfcfae713e5f78c146cfe4eec42e2b45df8aeaff
welschoff <h.welschoff@gmx.de>
2021-11-11 12:57:38 +0100

    :sparkles: Create custom hook useRecognizeText

--- src/app/components/AddDocumentForm/AddDocumentForm.tsx
+++ src/app/components/AddDocumentForm/AddDocumentForm.tsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import styles from './AddDocumentForm.styles.module.css';
 
-function AddDocumentForm() {
+type AddDocumentFormProps = {
+  text: string;
+};
+
+function AddDocumentForm({ text }: AddDocumentFormProps) {
   const [title, setTitle] = useState('');
 
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const document = {
+      title,
+      text,
+    };
+    fetch('https://json-server.machens.dev/docs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(document),
+    });
+    setTitle('');
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
         type="text"
         placeholder="Enter title"
@@ -13,7 +34,12 @@ function AddDocumentForm() {
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
-      <input type="submit" className={styles.submit} value="Add" />
+      <input
+        type="submit"
+        className={styles.submit}
+        value="Add"
+        disabled={!title}
+      />
     </form>
   );
 }
--- src/app/pages/scan/Scan.tsx
+++ src/app/pages/scan/Scan.tsx
@@ -1,48 +1,44 @@
 import React, { useState } from 'react';
 import ImageInput from '../../components/ImageInput/ImageInput';
 import styles from './Scan.module.css';
-import { RecognizeProgress, recognizeText } from '../../utils/ocr';
 import Progress from '../../components/Progress/Progress';
 import AddDocumentForm from '../../components/AddDocumentForm/AddDocumentForm';
+import useRecognizedText from '../../utils/UseRecognizedText';
 
 function Scan(): JSX.Element {
   const [imageURL, setImageURL] = useState<string | null>(null);
-  const [recognizedText, setRecognizedText] = useState<string | null>(null);
-  const [recognizeProgress, setRecognizeProgress] =
-    useState<RecognizeProgress | null>(null);
+  const { text, progress, recognize } = useRecognizedText();
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Docs Scanner</h1>
 
-      {recognizedText ? (
-        <p className={styles.text}>{recognizedText}</p>
+      {text ? (
+        <p className={styles.text}>{text}</p>
       ) : (
         <ImageInput onUpload={setImageURL} />
       )}
 
-      {recognizedText && <AddDocumentForm />}
+      {text && <AddDocumentForm text={text} />}
 
-      {!recognizedText && recognizeProgress && (
-        <Progress
-          progress={recognizeProgress.progress * 100}
-          status={recognizeProgress.status}
-        />
+      {!text && progress && (
+        <Progress progress={progress.progress * 100} status={progress.status} />
       )}
 
-      {!recognizeProgress && (
-        <button
-          disabled={imageURL === null}
-          onClick={() => {
-            if (imageURL) {
-              recognizeText(imageURL, setRecognizeProgress).then(
-                setRecognizedText
-              );
-            }
-          }}
-        >
-          Scan
-        </button>
+      {!progress && (
+        <div>
+          <button
+            disabled={imageURL === null}
+            onClick={() => {
+              if (imageURL) {
+                recognize(imageURL);
+              }
+            }}
+          >
+            Scan
+          </button>
+          <button className={styles.documents}>Documents</button>
+        </div>
       )}
     </div>
   );
