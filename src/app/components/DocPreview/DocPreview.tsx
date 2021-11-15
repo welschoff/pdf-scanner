c5a91f8e5767e684044ee4465be486be7e3410c2
welschoff <h.welschoff@gmx.de>
2021-11-15 11:00:58 +0100

    :sparkles: Add Read more/less button

--- src/app/components/DocPreview/DocPreview.tsx
+++ src/app/components/DocPreview/DocPreview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './DocPreview.module.css';
 
 export type Document = {
@@ -8,12 +8,34 @@ export type Document = {
 };
 
 function DocPreview({ title, text }: Document): JSX.Element {
-  return (
-    <div className={styles.wrapper}>
-      <h2>{title}</h2>
-      <p>{text}</p>
-    </div>
-  );
+  const [collapsed, setCollapsed] = useState(true);
+  //   return (
+  //     <div className={styles.wrapper}>
+  //       <h2>{title}</h2>
+  //       <p>{text}</p>
+  //     </div>
+  //   );
+
+  if (!text || text.length <= 10) {
+    return (
+      <article>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </article>
+    );
+  } else {
+    return (
+      <article>
+        <h2>{title}</h2>
+        <p>
+          {collapsed ? `${text.substring(0, 10)}...` : text}
+          <button onClick={() => setCollapsed(!collapsed)}>
+            Read {collapsed ? 'more' : 'less'}
+          </button>
+        </p>
+      </article>
+    );
+  }
 }
 
 export default DocPreview;
--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -4,12 +4,8 @@ import styles from './SearchBar.module.css';
 export default function SearchBar(): JSX.Element {
   return (
     <div>
-      <section>
-        <input
-          type="text"
-          placeholder="Searchbar"
-          className={styles.searchbar}
-        />
+      <section className={styles.searchbar}>
+        <input type="text" placeholder="Searchbar" />
         <input type="submit" value="Search" className={styles.submit} />
       </section>
     </div>
